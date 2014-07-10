1c9f373b2f6058da4fab1813b610c3044bd1008f
eeooz <hyzhang22@gmail.com>
2014-07-10 11:33:12 -0400

    tle

---
+++ triangle.py
@@ -0,0 +1,22 @@
+class Solution:
+    # @param triangle, a list of lists of integers
+    # @return an integer
+    def minimumTotal(self, triangle):
+        levels = len(triangle)
+        f = [ 0 for i in range(levels) ]
+    
+        for i in range(levels):
+            # a new copy
+            tmp = f[:]
+            for idx in range(i+1):
+                if idx==0:
+                    f[idx] = tmp[idx] + triangle[i][idx]
+                    print 'first', idx, f[idx],f
+                elif idx == i:
+                    f[idx] = tmp[idx-1]+triangle[i][idx]
+                    print 'second', idx, f[idx],f
+                else:
+                    f[idx] = min(tmp[idx], tmp[idx-1])+triangle[i][idx]
+
+        return max(f)
+                
