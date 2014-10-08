953dacb36e8475e93389a880848097243a982395
Zoe Zhang <hyzhang22@gmail.com>
2014-10-07 22:37:23 -0400

    added sparse matrix version; added transitional prob tweak function

--- pageRank.py
+++ pageRank.py
@@ -2,19 +2,12 @@ import random
 import math
 import sys
 import numpy as np
+
 # @param M: numpy array of adjacency matrix where M_ij represents the link from j to i such that sum(i,M_ij)=1 for all j
 # @param d: damping factor
 # @param thres: quadratic error threshold
-# @param quadraticErr: quadratic error for v
 # @return v: a vector of ranks such that v_i is the ith rank from [0, 1]
 def pageRank(M, d, thresh):
-
-    # check validity of M
-    for i in range(len(M[0])):
-        if sum(M[:][i])!=0:
-            print 'invalid M'
-            return
-
     N = len(M[0])
     # initialization of v
     # can use all equal probabilities v = np.array([1./N for i in range(N)])
@@ -31,5 +24,56 @@ def pageRank(M, d, thresh):
 
     return v
 
+
+# @param M: dictionary of non-zero elements in the adjacency matrix where key is the row number, value is tupe(col number,element value), assuming M is valid 
+# @param d: damping factor
+# @param thres: quadratic error threshold
+# @param num: number of links
+# @return v: a dictionary of ranks where key is the idex of a link, value is the rank measure
+def pageRankSparse(M, d, thresh, num):
+    # equal probability initialization
+    v = np.ones(num)*1./num
+    err  = sys.maxint
+
+    while err > thresh:
+        vPre = np.copy(v)
+        for key in M:
+            v[key] = sum([ d*elem[1]*v[elem[0]] for elem in M[key]])        
+            v[key]+= (1-d)*1./num 
+        err = math.sqrt(sum(abs(v-vPre)**2)) 
+
+    return v
+
+
+# @param M: numpy array of adjacency matrix where M_ij represents the link from j to i such that sum(i,M_ij)=1 for all j
+# @param idx: an int, index of the link to be tweaked
+# @param idxIncrease: an int, index of the link to be increased (or decreased)
+# @percentage: a float, percentage of increase in transitional probability (decrease if negative)
+def tweakM(M,idx,idxIncrease,increase):
+    if M[idxIncrease][idx]==0:
+        M[idxIncrease][idx] = delta =  increase
+    else:
+        delta = M[idxIncrease][idx]*increase 
+        M[idxIncrease][idx] += delta
+ 
+    for i in range(len(M)):
+        if i!=idxIncrease and M[i][idx] != 0:
+            M[i][idx] -= delta*M[i][idx]/(1-M[idxIncrease][idx]+delta)
+
+    return M        
+
+
 M = np.asarray([[0, 0, 0, 0, 1], [ 0.5, 0, 0, 0, 0], [0.5, 0, 0, 0, 0], [ 0, 1, 0.5, 0, 0],[0, 0, 0.5, 1, 0]])
 print pageRank(M, 0.8, 0.001)
+
+Msparse = {}
+Msparse[0] = [[4,1]]
+Msparse[1] = [[0,0.5]]
+Msparse[2] = [[0,0.5]]
+Msparse[3] = [[1,1],[2,0.5]]
+Msparse[4] = [[2,0.5],[3,1]]
+print pageRankSparse(Msparse, 0.8, 0.001, 5)
+
+Mtweak = tweakM(M,0,1,0.2)
+print Mtweak
+print pageRank(Mtweak, 0.8, 0.001)
