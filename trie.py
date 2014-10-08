fa21f80a05195eb130c0946b1fb36b7252b74fed
Zoe Zhang <hyzhang22@gmail.com>
2014-10-08 19:59:13 -0400

    first scratch

---
+++ trie.py
@@ -0,0 +1,56 @@
+class Node:
+    def __init__(self):
+        self.next = [None for i in range(26)]
+        self.val = False
+
+class Rtrie:
+    def __init__(self):
+        self.root = Node()
+        self.N = 0  # number of keys in the trie
+    
+    def size(self):
+        return self.N
+        
+    def isEmpty(self):
+        return self.size==0
+    
+    # @param node: root
+    # @param key: a string
+    # @param val: a Boolean
+    # @param d: an int
+    def putHelper(self, node, key, val, d):
+        if not node:
+            node = Node()
+        if d==len(key):
+            if not node.val:
+                self.N += 1
+            node.val = val
+            return node
+        c = key[d]
+        node.next[c] = self.put(node.next[c],key, val, d+1) 
+        return node       
+
+    def put(self, key, val):
+        if not val:
+            self.delete(key)
+        else:
+            self.root = self.putHelper(self.root, key, val, 0)
+    
+    # @param node: a Node
+    # @param key: a string
+    # @param d: an int
+    def getHelper(self,node,key,d):
+        if not node:
+            return None
+        if d==len(key):
+            return x
+        c = key[d]
+        return self.get(node.next[c], key, d+1)
+    def get(self, key):
+        node = self.getHelper(self.root, key, 0)
+        if not node:
+            return None
+        return node.val
+
+    def contains(self, key):
+        return self.get(key)!=None 
